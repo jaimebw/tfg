31c44e00a644efaf5db1039f0f8065a5932ed265
Jaime Bowen <jaimebwv@gmail.com>
2020-08-09 18:56:07 +0200

    Modifcaciones de formato

--- libros_a_scripts/descarga_bilbao.py
+++ libros_a_scripts/descarga_bilbao.py
@@ -1,10 +1,9 @@
-from traffic.data import opensky,nm_airspaces #se importan los recursos que se van a usar
-
+from traffic.data import opensky,nm_airspaces # se importan los recursos 
+                                              # que se van a usar
 
 flight = opensky.history(
     "2019-02-05", # fecha de inicia
     "2019-02-08", # fecha de parada
-    # stop is implicit, i.e. stop="2017-02-06"
-    return_flight=True, # define la clase de estructura en función de lo que se necesita
+    return_flight=True, # define la clase de objeto que se descarga en función de lo que se necesita
     bounds= nm_airspaces['LECMBLU'] # area de descarga de los datos
 )    
