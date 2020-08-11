96dcde0441c97922919fa4d95b09953223b7b71b
Jaime Bowen <jaimebwv@gmail.com>
2020-08-11 14:11:37 +0200

    Añadida funcion para nivel de vuelo

--- libros/biblio_herramienta/herramienta.py
+++ libros/biblio_herramienta/herramienta.py
@@ -1,3 +1,4 @@
+import traffic
 def guardarcsv(archivo, nombre_archivo,carpeta_datos_csv ):
     # esta funciona genera una carpeta sino existe, y guarda los datos descargados en esta
     if not os.path.exists(carpeta_datos_csv):
@@ -7,6 +8,7 @@ def guardarcsv(archivo, nombre_archivo,carpeta_datos_csv ):
     archivo.to_csv(carpeta_datos_csv+nombre_archivo+".csv")
 
 def probartraffic():
+    # prueba si las bibliotecas está bien instaladas
     try:
         import traffic
         print("Traffic instalado")
@@ -14,5 +16,14 @@ def probartraffic():
         print("La librería no esta correctamente instalada")
 
 def cargardatos(carpeta,nombre_datos):
+    # carga los datos descargados desde una carpeta
     datos_cargados = traffic.core.Traffic.from_file(carpeta+nombre_datos)
     return datos_cargados
+
+def niveldevuelo(altitud_metros):
+    # devuelve el nivel de vuelo 
+    try:
+        fl = altitud_metros*3.28/1000
+        return fl
+    except:
+        pass
