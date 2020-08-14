c2773597418cc6b183404c3c3f366e46b3dbb99c
Jaime Bowen <jaimebwv@gmail.com>
2020-08-14 14:17:16 +0200

    Quitadas funciones

--- libros/biblio_herramienta/herramienta.py
+++ libros/biblio_herramienta/herramienta.py
@@ -1,14 +1,6 @@
 import traffic
 from traffic.core import Traffic
 
-def guardarcsv(archivo, nombre_archivo,carpeta_datos_csv ):
-    # esta funciona genera una carpeta sino existe, y guarda los datos descargados en esta
-    if not os.path.exists(carpeta_datos_csv):
-        os.mkdir(carpeta_datos_csv)
-    # guarda el archivo .csv en la carpeta especificada
-    carpeta_datos_csv = r"datos_sectores/"
-    archivo.to_csv(carpeta_datos_csv+nombre_archivo+".csv")
-
 def probartraffic():
     # prueba si las bibliotecas está bien instaladas
     try:
@@ -17,10 +9,6 @@ def probartraffic():
     except:
         print("La librería no esta correctamente instalada")
 
-def cargardatos(carpeta,nombre_datos):
-    # carga los datos descargados desde una carpeta
-    datos_cargados = Traffic.from_file(carpeta+nombre_datos)
-    return datos_cargados
 
 def niveldevuelo(altitud_metros):
     # devuelve el nivel de vuelo 
