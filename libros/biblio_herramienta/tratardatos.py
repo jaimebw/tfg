ec26b8f85fb80ed234c9cc495d6ea427c7d0d66b
Jaime Bowen <jaimebwv@gmail.com>
2020-08-22 13:21:09 +0200

    Cambiado formato de ambos

--- libros/biblio_herramienta/tratardatos.py
+++ libros/biblio_herramienta/tratardatos.py
@@ -24,13 +24,20 @@ def guardarcsv(archivo, nombre_archivo,carpeta_datos_csv = "datos_sectores" ):
 
 def cargardatos(carpeta,nombre_datos):
     # carga los datos descargados desde una carpeta
+    Var_time = ["hour", "last_position", "timestamp"]
     datos_cargados = Traffic.from_file(carpeta+nombre_datos)
+    #datos_cargados.data = datos_cargados.data.drop(['Unnamed: 0'])
+    datos_cargados.data[Var_time] = datos_cargados.data[Var_time] = datos_cargados.data[Var_time].astype("datetime64[ns, UTC]")
     return datos_cargados
 
+"""
+def cargardatos(carpeta,nombre_datos):
+    # carga los datos descargados desde una carpeta
+    datos_cargados = Traffic.from_file(carpeta+nombre_datos)
+    return datos_cargados
+"""
 def cargardatosfiltrados(carpeta,nombre_datos):
     # carga los datos descargados desde una carpeta y pone formato de tiempos
-    
-    
     datos_cargados = Traffic.from_file(carpeta+nombre_datos)
     Var_time = ["hour", "last_position", "timestamp"]
     datos_cargados.data[Var_time] = datos_cargados.data[Var_time] = datos_cargados.data[Var_time].astype("datetime64[ns, UTC]")
