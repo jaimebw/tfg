1631c7b7da604aaa24fa7cd9792851523d63bce7
Jaime Bowen <jaimebwv@gmail.com>
2020-09-02 17:38:28 +0200

    Creado nuevo

---
+++ libros/biblio_herramienta/ml.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+def dividirdatos(datos, test_ratio):
+    np.random.seed(5) # esto es para que siempre genere los mismos datasets
+    # genera los data sets de entrenamiento y de prueba
+    shuffled_indices = np.random.permutation(len(datos))
+    test_set_size = int(len(datos) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return datos.iloc[train_indices], datos.iloc[test_indices]
